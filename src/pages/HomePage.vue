5862828ab844e0849edb72d93023a6172906557b
winA <92146550+a40418a@users.noreply.github.com>
2025-07-22 00:17:30 +0900

    [NHDR-93] feat: 모달박스 생성

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="text-primary-300 text-2xl font-bold">홈페이지</div>
+  <div>
+    <div class="text-xl font-bold">홈페이지</div>
+    <button @click="open = true" class="bg-primary-100 p-4">모달 열기</button>
+
+    <!-- 조건부 모달 -->
+    <Modal
+      v-if="open"
+      title="자산 정보 등록"
+      leftLabel="취소"
+      rightLabel="등록"
+      @click1="onCancel"
+      @click2="onSubmit"
+    >
+      <p class="h-62 text-left">여기에 입력 폼 혹은 콘텐츠를 넣으세요.</p>
+    </Modal>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import Modal from '@/components/modals/Modal.vue';
+
+const open = ref(false);
+
+const onCancel = () => {
+  open.value = false;
+};
+
+const onSubmit = () => {
+  alert('등록 처리!');
+  open.value = false;
+};
+</script>
