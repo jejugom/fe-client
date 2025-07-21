7ce535ebc8f640076cc82d0a5202f63b03efc8f3
saeun-park <ajsklaoao@naver.com>
2025-07-21 20:52:40 +0900

    [NHDR-73] feat: 노후 페이지 퍼블리싱

---
+++ src/pages/products/PersonalizedProducts.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="space-y-6 bg-white px-4">
+    <Banner></Banner>
+    <h2 class="mb-2 text-xl font-semibold text-blue-600"
+      >든든한 노후를 위한 맞춤형 금융상품</h2
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import Banner from '@/components/cards/Banner.vue';
+</script>
