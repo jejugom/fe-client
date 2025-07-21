c40a0fc5c3b6c0c460d057b44081243b507a7d62
saeun-park <ajsklaoao@naver.com>
2025-07-21 20:53:55 +0900

    [NHDR-73] feat: 컴포넌트 생성

---
+++ src/components/cards/Banner.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="mt-4 mb-4 flex h-36 items-center justify-center bg-green-500 p-6 text-white"
+  >
+    <p class="text-2xl font-bold">광고배너</p>
+  </div>
+</template>
+<script></script>
