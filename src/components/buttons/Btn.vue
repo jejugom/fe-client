2a2f886aa7c1d3adf7a80041f9cc0a92e6239fe5
winA <92146550+a40418a@users.noreply.github.com>
2025-07-21 15:24:34 +0900

    [NHDR-93] feat: 버튼컴포넌트생성

---
+++ src/components/buttons/Btn.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    :class="[
+      'rounded-lg font-semibold',
+      color === 'primary' && 'bg-primary-100 text-primary-500',
+      color === 'secondary' && 'bg-secondary-100 text-secondary-500',
+      color === 'secondary-stroke' &&
+        'bg-secondary-100 text-secondary-500 border-secondary-300 border',
+      color === 'surface' &&
+        'text-surface-500 border-surface-200 border bg-white',
+      size === 'small' && 'h-8 w-20 text-sm',
+      size === 'medium' && 'h-15 w-72 text-lg',
+      size === 'large' && 'h-15 w-full text-lg',
+      size === 'square' && 'h-13 w-13 text-sm',
+      $attrs.class,
+    ]"
+    v-bind="$attrs"
+    @click="onClick"
+    ><slot>{{ label }}</slot></button
+  >
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+});
+defineProps<{
+  label?: string;
+  color?: 'primary' | 'secondary' | 'secondary-stroke' | 'surface';
+  size?: 'small' | 'medium' | 'large' | 'square';
+}>();
+
+const emit = defineEmits<{
+  (e: 'click'): void;
+}>();
+
+const onClick = () => {
+  emit('click');
+};
+</script>
---
+++ src/components/buttons/BtnSet.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex gap-5 text-sm font-semibold">
+    <button
+      class="bg-secondary-100 text-secondary-500 h-10.5 w-33 rounded-lg"
+      @click="onClick1"
+    >
+      {{ label1 }}
+    </button>
+    <button
+      class="bg-primary-100 text-primary-500 h-10.5 w-33 rounded-lg"
+      @click="onClick2"
+    >
+      {{ label2 }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  label1?: string;
+  label2?: string;
+  onClick1?: () => void;
+  onClick2?: () => void;
+}>();
+</script>
