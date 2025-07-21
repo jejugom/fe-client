1fc3b85136ec9c54eb0d90975c2500dc48b13c96
winA <92146550+a40418a@users.noreply.github.com>
2025-07-21 15:33:32 +0900

    [NHDR-93] feat: input,select 생성

---
+++ src/components/forms/SelectBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <select
+    :class="[
+      'border-surface-300 rounded-lg border px-3 text-base',
+      size === 'large' && 'h-13 w-82.5',
+      size === 'medium' && 'h-13 w-68',
+      size === 'small' && 'h-10 w-25',
+      $attrs.class,
+    ]"
+    v-bind="$attrs"
+    :value="modelValue"
+    @change="$emit('update:modelValue', $event.target.value)"
+  >
+    <slot />
+  </select>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  modelValue?: string;
+  size?: 'small' | 'medium' | 'large';
+}>();
+
+defineEmits<{
+  (e: 'update:modelValue', value: string): void;
+}>();
+</script>
