cdb64111a5d7131653e2117e5b6895d08336aaf1
winA <92146550+a40418a@users.noreply.github.com>
2025-07-23 14:13:50 +0900

    [NHDR-109] feat: 기타페이지 퍼블리싱

--- src/pages/etc/ETL1_FailPage.vue
+++ src/pages/etc/ETL1_FailPage.vue
@@ -1,1 +1,31 @@
-<template>실패페이지</template>
+<template>
+  <div class="flex flex-col items-center justify-center gap-8">
+    <div class="text-8xl">😔</div>
+    <div class="text-center text-lg font-semibold"
+      >작업을 처리하는 데 문제가 발생했어요.<br />
+      불편을 드려 죄송합니다.<br />
+      걱정 마세요, 문제가 곧 해결될거예요.</div
+    >
+    <BtnSet
+      label1="이전 페이지로"
+      label2="홈 페이지로"
+      :onClick1="goBack"
+      :onClick2="goHome"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import BtnSet from '@/components/buttons/BtnSet.vue';
+
+const router = useRouter();
+
+const goBack = () => {
+  router.back();
+};
+
+const goHome = () => {
+  router.push({ name: 'home' });
+};
+</script>
--- src/pages/etc/ETL1_LoadingPage.vue
+++ src/pages/etc/ETL1_LoadingPage.vue
@@ -1,1 +1,16 @@
-<template>로딩페이지</template>
+<template>
+  <div class="flex flex-col items-center justify-center gap-8">
+    <div
+      class="border-gold h-10 w-10 animate-spin rounded-full border-4 border-t-transparent"
+    ></div>
+
+    <div class="text-center text-lg font-bold">
+      잠시만 기다려주세요. <br />
+      <span class="text-gold">
+        행복한 미래를<br />안전하게 설계 중입니다...
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
