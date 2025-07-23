e78096c8cf3f4826565dc87fa4d7c2f49972527e
saeun-park <ajsklaoao@naver.com>
2025-07-23 15:43:52 +0900

    [NHDR-98] feat: JE-L5-01-001 퍼블리싱

--- src/pages/gift/JEL5_GiftResultPage.vue
+++ src/pages/gift/JEL5_GiftResultPage.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>증여 결과 페이지</div>
+  <div class="flex-1 px-4"
+    ><h2 class="text-primary-300 mb-8 text-2xl font-bold"
+      >증여 시뮬레이션 결과</h2
+    >
+
+    <div
+      class="bg-gradient-cross-1 mb-4 flex h-80 w-full items-center justify-center"
+    >
+      <span class="text-surface-500">예시 이미지</span>
+    </div>
+    <div class="flex flex-col gap-3">
+      <Btn color="secondary" label="결과 공유하기" size="large" />
+      <Btn color="primary" label="내점 예약하기" size="large" />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Btn from '@/components/buttons/Btn.vue';
+</script>
 
 <style scoped></style>
