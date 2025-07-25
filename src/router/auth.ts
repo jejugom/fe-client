d357fe778808e44217ede8c555c8b65a32ab222a
guno-p <sushieatinggg@gmail.com>
2025-07-25 14:50:15 +0900

    [NHDR-120] feat: 튜토리얼 라우터 수정, main.css 주석처리 삭제

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,11 +1,5 @@
 import type { RouteRecordRaw } from 'vue-router';
 
-const authRoutes: RouteRecordRaw[] = [
-  {
-    path: '/tutorial',
-    name: 'tutorial',
-    component: () => import('@/pages/auth/TutorialPage.vue'),
-  },
-];
+const authRoutes: RouteRecordRaw[] = [];
 
 export default authRoutes;
