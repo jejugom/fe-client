409b97ceb0414b05654861d5fda5a4aef23d0ec7
winA <92146550+a40418a@users.noreply.github.com>
2025-07-27 15:24:06 +0900

    [NHDR-150] feat: 이벤트 페이지 작성

--- src/pages/event/EVL1_EventPage.vue
+++ src/pages/event/EVL1_EventPage.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="space-y-6 bg-white px-4">
-    <h2 class="mb-2 text-xl font-semibold text-blue-600">이벤트 페이지</h2>
+  <div class="space-y-16">
+    <div class="space-y-4">
+      <div class="text-primary-300 text-2xl font-bold">오늘의 도전</div>
+      <ChallengeCarousel />
+    </div>
+    <div class="space-y-4">
+      <div class="text-primary-300 bg-white text-2xl font-bold"
+        >챌린지 현황</div
+      >
+      <ChallengeState />
+      <Btn color="primary" label="보상 선택하기" size="large" />
+    </div>
   </div>
 </template>
-<script setup lang="ts">
-// 필요 시 여기에 코드 작성
-</script>
 
-<style scoped>
-/* 스타일 선택 */
-</style>
+<script setup lang="ts">
+import ChallengeCarousel from './_components/ChallengeCarousel.vue';
+import Btn from '@/components/buttons/Btn.vue';
+import ChallengeState from './_components/ChallengeState.vue';
+</script>
