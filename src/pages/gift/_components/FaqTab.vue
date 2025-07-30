bca052ec7ae8049ccb6b18a4b4b3f313f1984899
guno-p <sushieatinggg@gmail.com>
2025-07-30 12:44:59 +0900

    [NHDR-81] style: 색 변화 버튼 컴포넌트 수정

--- src/pages/gift/_components/FaqTab.vue
+++ src/pages/gift/_components/FaqTab.vue
@@ -3,10 +3,11 @@
     @click="$emit('click')"
     :class="[
       side === 'left' ? 'mr-2' : 'ml-2',
-      'w-1/2 rounded-xl px-4 py-3 text-base font-semibold transition-none',
+      'w-1/2 rounded-xl border px-4 py-3 text-base font-semibold transition-colors duration-300',
       isActive
-        ? 'bg-primary-100 text-primary-500'
-        : 'border-surface-200 text-surface-500 border',
+        ? // border-transparent 적용으로 전환 시 검은 테두리 보이지 않도록 수정
+          'bg-primary-100 text-primary-500 border-transparent'
+        : 'text-surface-500 border-surface-200',
     ]"
   >
     <slot />
