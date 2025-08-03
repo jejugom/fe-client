721723644de78f6cb22491553768a4d2c86fd1a1
winA <92146550+a40418a@users.noreply.github.com>
2025-08-03 11:41:08 +0900

    [NHDR-185] style: 아이콘 카드 버튼 형식화

--- src/components/cards/IconCard.vue
+++ src/components/cards/IconCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     :class="[
-      'flex h-30 w-full items-center justify-start gap-6 rounded-lg p-4',
+      'relative flex h-30 w-full items-center justify-start gap-6 rounded-lg p-4',
       color === 'primary' && 'stroke-primary bg-primary-100',
       color === 'secondary' && 'stroke-secondary bg-secondary-100',
       $attrs.class,
@@ -9,11 +9,19 @@
     v-bind="$attrs"
     @click="onClick"
   >
-    <!-- 아이콘 부분 -->
+    <!-- 우측 상단 고정 아이콘 -->
+    <img
+      :src="ArrowIcon"
+      class="absolute top-4 right-4 h-6 w-6"
+      alt="arrow icon"
+    />
+
+    <!-- 아이콘 영역 -->
     <div class="flex h-16 w-16 items-center justify-center">
       <slot name="icon" />
     </div>
-    <!-- 글자 부분 -->
+
+    <!-- 텍스트 -->
     <div class="text-primary-300 flex flex-col gap-1">
       <div>{{ content1 }}</div>
       <div class="text-primary-500 text-2xl font-bold">{{ title }}</div>
@@ -21,8 +29,8 @@
     </div>
   </div>
 </template>
-
 <script setup lang="ts">
+import ArrowIcon from '@/assets/icons/Arrow45.svg';
 defineOptions({ inheritAttrs: false });
 defineProps<{
   title?: string;
