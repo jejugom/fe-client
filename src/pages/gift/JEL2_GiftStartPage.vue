de77bdac2eafbacdec7a8a408bcae502c8d83ba6
saeun-park <ajsklaoao@naver.com>
2025-08-04 20:11:22 +0900

    [NHDR-194] style: 증여/상속 초기페이지 구성 및 css 수정

--- src/pages/gift/JEL2_GiftStartPage.vue
+++ src/pages/gift/JEL2_GiftStartPage.vue
@@ -1,28 +1,19 @@
 <template>
   <!-- 제목 -->
-  <h2 class="text-primary-300 mb-2 text-2xl font-bold">상속·증여</h2>
-  <p class="text-surface-500 mb-8 text-base">
-    자녀나 손주에게 재산을 전하고 싶으신가요? <br />
-    세금이 얼마나 나올지 미리 확인해보세요.
+  <h2 class="text-primary-300 mb-2 text-2xl font-bold">증여 시뮬레이션 안내</h2>
+  <p class="text-surface-500 mb-8">
+    복잡한 증여 세금, 어렵게만 느껴지시나요? <br />
+    간단한 입력만으로 세금을 예측하고 절세 전략까지 확인해보세요.
   </p>
 
-  <!-- 예시 이미지 영역 -->
-  <div
-    class="bg-gradient-cross-1 mb-16 flex h-80 w-full items-center justify-center"
-  >
-    <span class="text-surface-500">예시 이미지</span>
-  </div>
+  <!-- 시뮬레이션 설명 카드 영역 -->
+  <SimulationGuide :guideList="GiftGuideList" />
 
   <!-- 하단 고정 안내 + 버튼 -->
-  <div
-    class="fixed bottom-[calc(5rem+1rem)] left-1/2 box-border w-full max-w-150 -translate-x-1/2 px-5"
-  >
-    <p class="text-primary-300 mb-2 text-center font-semibold">
-      언제, 누구에게 얼마나 주면<br />세금이 가장 적게 나올지 미리 알 수 있어요.
-    </p>
+  <div>
     <Btn
       color="primary"
-      label="증여 계획 미리 계산해보기"
+      label="수증자 정보 입력하기"
       size="large"
       @click="goToInput"
     />
@@ -32,6 +23,8 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
 import Btn from '@/components/buttons/Btn.vue';
+import SimulationGuide from './_components/SimulationGuide.vue';
+import { GiftGuideList } from './SimulationGuideData';
 
 const router = useRouter();
 
