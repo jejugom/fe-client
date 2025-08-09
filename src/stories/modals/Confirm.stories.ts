e7b650bef9a56e0f7c2a5382e5b6b7ad1ae8e674
winA <92146550+a40418a@users.noreply.github.com>
2025-08-10 02:12:44 +0900

    [NHDR-222] style: 알럿,컨펌창 생성

--- src/stories/modals/Confirm.stories.ts
+++ src/stories/modals/Confirm.stories.ts
@@ -1,15 +1,16 @@
 import type { Meta, StoryObj } from '@storybook/vue3-vite';
-import Alert from '@/components/modals/Alert.vue';
+import Confirm from '@/components/modals/Confirm.vue';
 
-const meta: Meta<typeof Alert> = {
-  title: 'modals/Alert',
-  component: Alert,
+const meta: Meta<typeof Confirm> = {
+  title: 'modals/Confirm',
+  component: Confirm,
   tags: ['autodocs'],
   argTypes: {
     title: { control: 'text' },
     leftLabel: { control: 'text' },
     rightLabel: { control: 'text' },
-    onClick: { action: 'click' },
+    onClick1: { action: 'click1' },
+    onClick2: { action: 'click2' },
   },
   args: {
     title: '모달 제목',
@@ -23,14 +24,14 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   render: (args) => ({
-    components: { Alert },
+    components: { Confirm },
     setup() {
       return { args };
     },
     template: `
-      <Alert v-bind="args" @click="alert('확인 클릭')">
+      <Confirm v-bind="args" @click1="alert('취소 클릭')" @click2="alert('확인 클릭')">
         <div class="h-52 text-center">모달 내용입니다. 여기에 원하는 내용을 넣으세요.</div>
-      </Alert>
+      </Confirm>
     `,
   }),
 };
